3d3da448461e93021e061df9b1b58751e980ba21
GUO Yao <yao.guo@student.ecp.fr>
2017-04-04 09:45:03 +0200

    update data processing 3d

--- data_processing3D.py
+++ data_processing3D.py
@@ -3,6 +3,7 @@
 """
 Created on Tue Feb 14 16:09:51 2017
 
+description: pack data
 @author: yaoyaoyao
 """
 
@@ -13,20 +14,19 @@ import math
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 # Change this to wherever you are storing your data:
 # IF YOU ARE FOLLOWING ON KAGGLE, YOU CAN ONLY PLAY WITH THE SAMPLE DATA, WHICH IS MUCH SMALLER
 
-IMG_PX_SIZE = 150
-HM_SLICES = 20
 
-data_dir = './data/sample_images/'
+data_dir = '/media/yaoyaoyao/Documents/Data/stage1/'
 patients = os.listdir(data_dir)
 labels = pd.read_csv('./data/stage1_labels.csv', index_col=0)
 
 
 
 
-IMG_SIZE_PX = 50
+IMG_SIZE_PX = 100
 SLICE_COUNT = 20
 
 def chunks(l, n):
@@ -63,6 +63,11 @@ def process_data(patient,labels_df,img_px_size=50, hm_slices=20, visualize=False
         new_slices.append(new_slices[-1])
         new_slices.append(new_slices[-1])
 
+    if len(new_slices) == hm_slices-3:
+        new_slices.append(new_slices[-1])
+        new_slices.append(new_slices[-1])
+        new_slices.append(new_slices[-1])
+
     if len(new_slices) == hm_slices+2:
         new_val = list(map(mean, zip(*[new_slices[hm_slices-1],new_slices[hm_slices],])))
         del new_slices[hm_slices]
@@ -82,11 +87,56 @@ def process_data(patient,labels_df,img_px_size=50, hm_slices=20, visualize=False
         
     return np.array(new_slices),label
 
-#                                               stage 1 for real.
+def process_data_without_label(patient,img_px_size=50, hm_slices=20, visualize=False):    
+    path = data_dir + patient
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+
+    new_slices = []
+    slices = [cv2.resize(np.array(each_slice.pixel_array),(img_px_size,img_px_size)) for each_slice in slices]
+    
+    chunk_sizes = math.ceil(len(slices) / hm_slices)
+    for slice_chunk in chunks(slices, chunk_sizes):
+        slice_chunk = list(map(mean, zip(*slice_chunk)))
+        new_slices.append(slice_chunk)
+
+    if len(new_slices) == hm_slices-1:
+        new_slices.append(new_slices[-1])
+
+    if len(new_slices) == hm_slices-2:
+        new_slices.append(new_slices[-1])
+        new_slices.append(new_slices[-1])
+
+    if len(new_slices) == hm_slices-3:
+        new_slices.append(new_slices[-1])
+        new_slices.append(new_slices[-1])
+        new_slices.append(new_slices[-1])
+
+    if len(new_slices) == hm_slices+2:
+        new_val = list(map(mean, zip(*[new_slices[hm_slices-1],new_slices[hm_slices],])))
+        del new_slices[hm_slices]
+        new_slices[hm_slices-1] = new_val
+        
+    if len(new_slices) == hm_slices+1:
+        new_val = list(map(mean, zip(*[new_slices[hm_slices-1],new_slices[hm_slices],])))
+        del new_slices[hm_slices]
+        new_slices[hm_slices-1] = new_val
+
+    if visualize:
+        fig = plt.figure()
+        for num,each_slice in enumerate(new_slices):
+            y = fig.add_subplot(4,5,num+1)
+            y.imshow(each_slice, cmap='gray')
+        plt.show()
+        
+    return np.array(new_slices)
+
+# stage 1 for real.
 
 
 x_train = []
 y_train = []
+x_test = []
 for num,patient in enumerate(patients):
     if num % 100 == 0:
         print(num)
@@ -95,8 +145,17 @@ for num,patient in enumerate(patients):
         x_train.append([img_data])
         y_train.append([label])
     except KeyError as e:
-        print('This is unlabeled data!')
+        print('This is unlabeled data! It belongs to x_test!')
+        img_data_without_label=process_data_without_label(patient,img_px_size=IMG_SIZE_PX, hm_slices=SLICE_COUNT)
+        x_test.append((patient,img_data_without_label))
+
 
 print("Data Processing Done!")
-np.save('./data/x_train-{}-{}-{}.npy'.format(IMG_SIZE_PX,IMG_SIZE_PX,SLICE_COUNT), x_train)
-np.save('./data/y_train-{}-{}-{}.npy'.format(IMG_SIZE_PX,IMG_SIZE_PX,SLICE_COUNT), y_train)
+print("Data saving!")
+with open('./data/preprocessing/data_processing3D.19.Mars.2/x_test-{}-{}-{}.pkl'.format(IMG_SIZE_PX,IMG_SIZE_PX,SLICE_COUNT),'wb') as f:
+    pickle.dump(x_test,f)
+with open('./data/preprocessing/data_processing3D.19.Mars.2/x_train-{}-{}-{}.pkl'.format(IMG_SIZE_PX,IMG_SIZE_PX,SLICE_COUNT),'wb') as f:
+    pickle.dump(x_train,f)
+with open('./data/preprocessing/data_processing3D.19.Mars.2/y_train-{}-{}-{}.pkl'.format(IMG_SIZE_PX,IMG_SIZE_PX,SLICE_COUNT),'wb') as f:
+    pickle.dump(y_train,f)
+print("All Done!")
